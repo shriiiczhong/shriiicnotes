0fe96b6e643e91c8d41f3f0d4b620f6cfa959f8f
shriiiczhong <czhong71@usc.edu>
2023-10-10 22:30:29 -0700

    Setting up our React app

---
+++ packages/frontend/src/containers/NotFound.tsx
@@ -0,0 +1,9 @@
+import "./NotFound.css";
+
+export default function NotFound() {
+  return (
+    <div className="NotFound text-center">
+      <h3>Oh shit! What's the fucking pages?</h3>
+    </div>
+  );
+}
